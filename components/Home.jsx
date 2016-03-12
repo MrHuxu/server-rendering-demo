aad4313f16ae07b30611f310085eadc5eabad182
xhu <hxtheone@gmail.com>
2016-03-12 07:57:52 -0800

    try to make dynamic work

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class Home extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      color: '#e57373'
+    }
+
+    this.setBlue = this.setBlue.bind(this);
+    this.setOrange = this.setOrange.bind(this);
+  }
+
+  setBlue () {
+    alert('hehe');
+    this.setState({ color: '#02bad2' })
+  }
+
+  setOrange () {
+    this.setState({ color: '#e57373' })
+  }
+
+  componentDidMount () {
+    alert('hehe')
+  }
+
   render () {
     return this.props.children || (
       <div>
         <h3> Home </h3>
+        <button onClick={this.setBlue}> set blue </button>
+        <button onClick={this.setOrange}> set orange </button>
+        <div style={{ width: 100, height: 50, backgroundColor: this.state.color }} />
         <a href='/test/'>to Test</a>
       </div>
     );
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+import path from 'path';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import routes from './routes/index';
 
 import express from 'express';
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use((req, res) => {
   // Note that req.url here should be the full URL path from
@@ -18,7 +22,9 @@ app.use((req, res) => {
       // You can also check renderProps.components or renderProps.routes for
       // your "not found" component or route respectively, and send a 404 as
       // below, if you're using a catch-all route.
-      res.status(200).send(renderToString(<RouterContext {...renderProps} />))
+      res.status(200).render('index', {
+        markup: renderToStaticMarkup(<RouterContext {...renderProps} />)
+      });
     } else {
       res.status(404).send('Not found')
     }
